1875b0676ff341da52515a15388847fa359451a2
Gautam Anand <gautamgxtv@gmail.com>
2023-10-07 17:20:10 -0500

    cool stuff

--- client/main.js
+++ client/main.js
@@ -1,7 +1,141 @@
+class Player {
+  constructor(id, x, y, app) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      this.app = app;
+      this.targetX = x;
+      this.targetY = y;
+      this.graphics = new PIXI.Graphics();
+      this.draw();
+  }
+
+  draw() {
+      this.graphics.clear();
+      this.graphics.beginFill(this.id === socket.id ? 0xFF0000 : 0x0000FF);  // Current player in red, others in blue
+      this.graphics.drawRect(0, 0, 50, 50);
+      this.graphics.endFill();
+      this.graphics.x = this.x;
+      this.graphics.y = this.y;
+      this.app.stage.addChild(this.graphics);
+  }
+
+  updatePosition(x, y) {
+    console.log('updatePosition', x, y);
+      this.targetX = x;
+      this.targetY = y;
+  }
+  interpolatePosition() {
+    const lerpSpeed = 0.1;  // Adjust this value for faster/slower interpolation
+    console.log('interpolatePosition', this.targetX, this.targetY);
+    this.x += (this.targetX - this.x) * lerpSpeed;
+    this.y += (this.targetY - this.y) * lerpSpeed;
+    this.draw();
+}
+}
+
 import io from 'socket.io-client';
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0xAAAAAA
+});
+document.body.appendChild(app.view);
+
+const players = {};
 
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
-    console.log('Connected to server!');
+  console.log('Connected to server!');
 });
+
+const activeKeys = {};
+
+document.addEventListener('keydown', (event) => {
+  switch(event.keyCode) {
+      case 37: // Left
+          activeKeys['left'] = true;
+          break;
+      case 38: // Up
+          activeKeys['up'] = true;
+          break;
+      case 39: // Right
+          activeKeys['right'] = true;
+          break;
+      case 40: // Down
+          activeKeys['down'] = true;
+          break;
+  }
+  emitPlayerMovement();
+});
+
+document.addEventListener('keyup', (event) => {
+  switch(event.keyCode) {
+      case 37: // Left
+          activeKeys['left'] = false;
+          break;
+      case 38: // Up
+          activeKeys['up'] = false;
+          break;
+      case 39: // Right
+          activeKeys['right'] = false;
+          break;
+      case 40: // Down
+          activeKeys['down'] = false;
+          break;
+  }
+  emitPlayerMovement();
+});
+
+function emitPlayerMovement() {
+  const directions = [];
+  for (let key in activeKeys) {
+      if (activeKeys[key]) directions.push(key);
+  }
+  if (directions.length > 0) {
+      socket.emit('move', directions);
+  }
+}
+
+socket.on('players', ({updatedPlayers, ball}) => {
+  // Clear previous player graphics
+  for (let id in players) {
+      players[id].graphics.clear();
+  }
+
+  // Update or create new players based on data from server
+  for (let id in updatedPlayers) {
+      if (players[id]) {
+          players[id].updatePosition(updatedPlayers[id].x, updatedPlayers[id].y);
+      } else {
+          players[id] = new Player(id, updatedPlayers[id].x, updatedPlayers[id].y, app);
+      }
+  }
+
+  handleSoccerBall(ball);
+});
+
+let soccerBallGraphics = new PIXI.Graphics();
+
+function handleSoccerBall(ballData) {
+    soccerBallGraphics.clear();
+    soccerBallGraphics.beginFill(0x000000);  // Black color
+    soccerBallGraphics.drawCircle(ballData.x, ballData.y, 25);
+    soccerBallGraphics.endFill();
+    app.stage.addChild(soccerBallGraphics);
+}
+
+
+app.ticker.add(() => {
+  // Interpolate player positions
+  for (let id in players) {
+      players[id].interpolatePosition();
+  }
+
+  // Check active keys and send movement
+  emitPlayerMovement();
+});
+
--- index.js
+++ index.js
@@ -6,15 +6,126 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('dist'));
+app.use(express.static('dist'));  // Assuming you're serving client files from "dist"
+
+class Player {
+    constructor(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const players = {};
+
+class SoccerBall {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.velocityX = 0;
+        this.velocityY = 0;
+    }
+
+    updatePosition() {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+        // Apply friction
+        this.velocityX *= 0.98;
+        this.velocityY *= 0.98;
+    }
+}
+
+const soccerBall = new SoccerBall(400, 300);  // Initialize soccer ball in the center
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected:', socket.id);
+
+    // Create a new player and add to the players object
+    players[socket.id] = new Player(socket.id, 400, 300);
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected:', socket.id);
+        delete players[socket.id];
+    });
+
+    // Handle player movement
+    socket.on('move', (directions) => {
+        const player = players[socket.id];
+        const speed = 5;
+        directions.forEach(direction => {
+            switch(direction) {
+                case 'left':
+                    player.x -= speed;
+                    break;
+                case 'up':
+                    player.y -= speed;
+                    break;
+                case 'right':
+                    player.x += speed;
+                    break;
+                case 'down':
+                    player.y += speed;
+                    break;
+            }
+        });
+
+        handleCollisions(player);
     });
 });
+
+function handleCollisions(player) {
+    const halfSize = 25;  // Assuming each square has a size of 50
+
+    // Check collision with other players
+    for (let otherId in players) {
+        if (otherId !== player.id) {
+            const other = players[otherId];
+            const collisionX = player.x + halfSize > other.x - halfSize && player.x - halfSize < other.x + halfSize;
+            const collisionY = player.y + halfSize > other.y - halfSize && player.y - halfSize < other.y + halfSize;
+
+            if (collisionX && collisionY) {
+                const overlapX = (halfSize * 2) - Math.abs(player.x - other.x);
+                const overlapY = (halfSize * 2) - Math.abs(player.y - other.y);
+
+                if (overlapX < overlapY) {
+                    if (player.x < other.x) {
+                        other.x += overlapX;
+                    } else {
+                        other.x -= overlapX;
+                    }
+                } else {
+                    if (player.y < other.y) {
+                        other.y += overlapY;
+                    } else {
+                        other.y -= overlapY;
+                    }
+                }
+            }
+        }
+    }
+
+    // Check collision with soccer ball
+    const ballSize = 25;
+    const collisionX = player.x + halfSize > soccerBall.x - ballSize && player.x - halfSize < soccerBall.x + ballSize;
+    const collisionY = player.y + halfSize > soccerBall.y - ballSize && player.y - halfSize < soccerBall.y + ballSize;
+
+    if (collisionX && collisionY) {
+        const dx = soccerBall.x - player.x;
+        const dy = soccerBall.y - player.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const force = 5;
+
+        soccerBall.velocityX = (dx / distance) * force;
+        soccerBall.velocityY = (dy / distance) * force;
+    }
+
+    soccerBall.updatePosition();
+}
+
+// Send periodic updates to all clients
+setInterval(() => {
+    io.emit('players', {updatedPlayers: players, ball: soccerBall});
+}, 1000/20);
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
