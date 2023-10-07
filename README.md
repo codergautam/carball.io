c9d9441bf2b0f24a3ddf7237d6e20913757af278
Gautam Anand <gautamgxtv@gmail.com>
2023-10-07 16:26:30 -0500

    Restart

--- README.md
+++
@@ -1,3 +0,0 @@
-# carball.io
-
-A project by **gautam**
--- src/server/helpers/idgen.js
+++
@@ -1,9 +0,0 @@
-function gen() {
-  let id = 0;
-  return () => {
-    id += 1;
-    return id;
-  };
-}
-const curGen = gen();
-module.exports = curGen;
