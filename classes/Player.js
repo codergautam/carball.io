717359408897a2eb3c1c1253ecb2d574b9083937
Gautam Anand <gautamgxtv@gmail.com>
2024-02-10 17:06:27 -0600

    decr torque

--- classes/Player.js
+++ classes/Player.js
@@ -63,7 +63,7 @@ module.exports = class Player {
     }
     updatePosition() {
         let body = this.body;
-        const torque = 500;
+        const torque = 400;
 
         if(typeof this.movement.angle == "number")
             this.updateRotation(torque);
