8d08485e650c94630c8b6ddb2969980770004ccd
Gautam Anand <gautamgxtv@gmail.com>
2024-02-10 20:11:22 -0600

    dfg

--- client/components/PlayerObject.js
+++ client/components/PlayerObject.js
@@ -24,7 +24,7 @@ export default class PlayerObject {
         const dimensions = [160, 90]
         this.matterBody = Matter.Bodies.rectangle(100, 100, dimensions[0]*0.95, dimensions[1]*0.95, {
             mass: 5,
-            restitution: 1.6,
+            restitution: 1,
             //friction: 0.1,  THIS is friction with other objects
             frictionAir: 0.07,
             //frictionStatic: 0.5  THIS is friction with other objects
